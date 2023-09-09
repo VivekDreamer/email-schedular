f62e8605dc6db92a3a8472ec7c9140ec7fafc3dd
VivekDreamer <vivekmakv@gmail.com>
2023-09-09 06:02:29 +0000

    Request and Response classes are added.

---
+++ src/main/java/com/example/vivek/emailschedular/payload/EmailResponse.java
@@ -0,0 +1,16 @@
+package com.example.vivek.emailschedular.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class EmailResponse {
+    @NonNull
+    private boolean success;
+    private String jobId;
+    private String jobGroup;
+    @NonNull
+    private String message;
+}
